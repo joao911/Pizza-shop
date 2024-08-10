3539a6ae87fe325f0dbf51265860e5d3d33f9960
Joao Paulo <joaopaulolacerda911@gmail.com>
2024-08-10 03:04:13 -0300

    feat: crated component pagination

--- src/screens/sigIn/DashBoard/Orders/index.tsx
+++ src/screens/sigIn/DashBoard/Orders/index.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-// import { Container } from './styles';
+import { OrderTableRow } from "./components/OrderTableRow";
+import { OrderTableFilter } from "./components/OrderTableFilter";
+import { Pagination } from "@/components/Pagination";
 
 export const Orders: React.FC = () => {
-  return <p>eu sou a pagina de pedidos</p>;
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
+
+        <div className="space-y-2.5">
+          <OrderTableFilter />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <OrderTableRow />
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={100} perPage={10} />
+        </div>
+      </div>
+    </>
+  );
 };
